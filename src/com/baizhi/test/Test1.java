14231a4fba202b1fe791d480ef9e8b3d0ece666d
dada <287760614@qq.com>
2018-11-26 22:42:48 +0800

    晚上的修改

--- src/com/baizhi/test/Test1.java
+++ src/com/baizhi/test/Test1.java
@@ -1,5 +1,7 @@
 package com.baizhi.test;
 
 public class Test1 {
-
+    public static void main(String args[]){
+        System.out.println("这是我的第一个程序");
+    }
 }
---
+++ src/com/baizhi/test/Test2.java
@@ -0,0 +1,8 @@
+package com.baizhi.test;
+
+public class Test2 {
+    public void test(){
+        System.out.println("发顺丰");
+        System.out.println("发顺丰");
+    }
+}
